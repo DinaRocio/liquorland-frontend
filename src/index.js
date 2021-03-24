3c7feda569f380f3ff372b85fa7bba6fe341508b
Dina Rocio <dinarociovq15@hotmail.com>
2021-03-23 21:07:20 -0500

    Added global styles for root, input

--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Global, css } from "@emotion/react";
+import { colors } from "./ui";
+
 import reportWebVitals from './reportWebVitals';
+
+const fontFamily = "ABeeZee, sans-serif";
+
+const globalStyles = css`
+ @import url('https://fonts.googleapis.com/css2?family=ABeeZee:ital@0;1&display=swap');
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background: grey;
+    font-family: ${fontFamily};
+    color: ${colors.dark1};
+  }
+  #root {
+    width: 360px;
+    background: linear-gradient(180deg, #ffffff 0%, #f2f2f2 100%);
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    padding: 32px;
+    margin: auto;
+    position: relative;
+  }
+
+  input {
+    font-family: inherit;
+    color: inherit;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
