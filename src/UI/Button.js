79489a19c8541de845063b172c46dbb83934c6ee
dianaceciliallc <dianaceciliallerenacastro@gmail.com>
2021-03-24 09:43:58 -0500

    Add dependencies and file for button component

---
+++ src/UI/Button.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const StyledButton = styled.button`
+
+`;
+
+function Button(){
+ return(
+    <StyledButton></StyledButton>
+ );
+}
+
+export default Button;
