4579f771aa16521384469a8f498ee4582d57dee0
dianaceciliallc <dianaceciliallerenacastro@gmail.com>
2021-03-25 12:33:43 -0500

    Add the page explore with styles according to figma and testing the component cardCategory

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,16 +1,24 @@
 import CardCategory from "../components/CardCategory"
 import Template from "../templates/Template"
 import Search from "../components/Search"
+import styled from "@emotion/styled";
 
 export default function Explore() {
   return (
     <Template>
-      <p>Find Products</p>
+      <Heading>Find Products</Heading>
       <Search/>
-      <CardCategory
-        
-      
-      />
+      <CardCategory/>
     </Template>
   );
 }
+
+const Heading = styled.h3`
+    font-family: ABeeZee;
+    font-style: italic;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 18px;
+    text-align:center;
+    margin-bottom:10px;
+`;
