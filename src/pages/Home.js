21a887be788ba7f9f2ea9083a3e364f5ca6123a7
dianaceciliallc <dianaceciliallerenacastro@gmail.com>
2021-03-25 00:43:19 -0500

    Add the search component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import Template from "../templates/Template";
+import Search from "../components/Search";
 
 export default function Home() {
   return (
     <Template>
       <p>Home</p>
+      <Search/>
     </Template>
   );
 }
