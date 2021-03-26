ce190d25c2aa86b6fe1446b4f93cf8dc36afe0fa
Dina Rocio <dinarociovq15@hotmail.com>
2021-03-26 13:38:40 -0500

    Add search icon to search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,22 +4,33 @@ import Icon from "../UI/Icon"
 
 function Search(){
     return(
+        <SearchBar>
+        <Icon type="search" fill="black" size={18} />
         <StyledInput
             placeholder="Search store"
             type="search"
         />
+        </SearchBar>
     );
 }
 
 export default Search;
 
+const SearchBar = styled.div`
+& > svg {
+    position: absolute;
+top: 107px;
+left: 52px;
+}
+`
+
 const StyledInput = styled.input`
-    width: 315px;
+    width: 100%;
     height: 29px;
     border:none;
     border-radius: 7px;
     background-color: ${colors.gray2};
-    padding:8px 18px;
+    padding: 18px 18px 18px 37px;
     outline:none;
     font-family: Abel;
     font-style: normal;
