cc96501a83b44809b8524a00e4270b81faf7e94f
Frank Dominguez <elvisf.dominguez@gmail.com>
2021-03-27 17:57:12 -0500

    Create Product screen and add media queries to ui.js it will be a reference to responsive design - Product view needs to finish its styles.

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,181 @@
+import styled from "@emotion/styled";
+import { colors, screenMediaQueries, screenSizes } from "../ui";
+import Button from "../UI/Button";
+import { FaChevronLeft, FaMinus, FaPlus } from "react-icons/fa";
+import { BsHeartFill, BsHeart } from "react-icons/bs";
+
+export default function ProductDetail() {
+  return (
+    <TemplateOne>
+      <ContainerStyled>
+        <NavToStyled>
+          <FaChevronLeft className="icon-button" />
+        </NavToStyled>
+        <Image src="https://www.chicagotribune.com/resizer/BIvQ3G9tzipdSyEA6eudrC5poGA=/415x233/top/www.trbimg.com/img-5c8fff83/turbine/ct-1552940928-jrrojmfcdk-snap-image" />
+        <HeadStyled>
+          <TitleStyled>Natural red Apple</TitleStyled>
+          {false ? <BsHeartFill color="red" /> : <BsHeart />}
+          <SubTitleGrayStyled>150 ml.</SubTitleGrayStyled>
+        </HeadStyled>
+        <AmountContainer>
+          <div className="change-quantity">
+            <FaMinus className="icon-button" />
+            <input value="1" onChange={() => {}} />
+            <FaPlus className="icon-button" />
+          </div>
+          <TextM>$20.00</TextM>
+        </AmountContainer>
+        <ProductDetailStyled>
+          Priduct Detail Eiusmod enim cupidatat sint cillum quis ut amet ullamco
+          minim Lorem dolor eiusmod ex nulla. Nisi proident dolor voluptate enim
+          nostrud non ex. Enim do quis officia culpa dolore amet quis. In nisi
+          ex ut aliquip cupidatat ut est magna sunt nisi ex. Ullamco in sint
+          laboris do cillum.
+        </ProductDetailStyled>
+      </ContainerStyled>
+      <FooterStyled>
+        <Button>Add to Basquet</Button>
+      </FooterStyled>
+    </TemplateOne>
+  );
+}
+
+const TemplateOne = styled.div`
+  position: absolute;
+  top: 0;
+  margin: 0 auto;
+  height: 100vh;
+  width: 100%;
+
+  display: grid;
+  top: 0;
+  grid-template-rows: calc(100% - 75px) 75px;
+
+  ${screenMediaQueries.sm} {
+    width: 95%;
+  }
+  ${screenMediaQueries.md} {
+    width: 90%;
+  }
+  ${screenMediaQueries.lg} {
+    width: 100%;
+  }
+  ${screenMediaQueries.xl} {
+    max-width: ${screenSizes.xl};
+  }
+`;
+
+const ContainerStyled = styled.div`
+  height: 100%;
+  overflow-y: scroll;
+  padding: 0 25px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+
+  .icon-button {
+    font-size: 20px;
+    color: ${colors.dark0};
+    cursor: pointer;
+  }
+`;
+
+const FooterStyled = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 25px;
+  * {
+    width: 100%;
+  }
+`;
+
+const NavToStyled = styled.nav`
+  font-size: 18px;
+  padding: 10px 0px;
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  height: auto;
+  max-height: 350px;
+  border: none;
+  object-fit: contain;
+  border-radius: 8px;
+  align-self: center;
+`;
+
+const HeadStyled = styled.div`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  gap: 10.5px 4px;
+`;
+
+const TitleStyled = styled.h2`
+  font-style: italic;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 18px;
+  letter-spacing: 0.1px;
+  color: ${colors.dark0};
+`;
+
+const SubTitleStyled = styled.h4`
+  font-family: Abel;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 18px;
+  color: ${colors.dark0};
+`;
+
+const SubTitleGrayStyled = styled(SubTitleStyled)`
+  color: ${colors.gray};
+`;
+
+const TextM = styled.p`
+  font-family: ABeeZee;
+  font-style: italic;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 18px;
+
+  letter-spacing: 0.1px;
+  color: ${colors.dark0};
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .change-quantity {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    font-size: 18px;
+    color: ${colors.dark0};
+
+    input {
+      width: 45.67px;
+      height: 45.67px;
+
+      border: 1px solid ${colors.gray};
+      box-sizing: border-box;
+      border-radius: 17px;
+
+      font-family: Abel;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 18px;
+      line-height: 18px;
+
+      /* identical to box height, or 100% */
+      display: flex;
+      text-align: center;
+
+      color: ${colors.dark0};
+    }
+  }
+`;
+
+const ProductDetailStyled = styled.main``;
