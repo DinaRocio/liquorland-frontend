afb7f5279a6d22ae001fd0e80d680424ed608417
dianaceciliallc <dianaceciliallerenacastro@gmail.com>
2021-03-27 18:03:23 -0500

    Adding the component search for desktop screen according to figma

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -2,14 +2,20 @@ import CardCategory from "../components/CardCategory"
 import Template from "../templates/Template"
 import Search from "../components/Search"
 import styled from "@emotion/styled";
+import Navbar from "../UI/Navbar"
 
 export default function Explore() {
   return (
-    <Template>
-      <Heading>Find Products</Heading>
+
+    <div>
+      <Navbar/>
       <Search/>
-      <CardCategory/>
-    </Template>
+    </div>
+    // <Template>
+    //   <Heading>Find Products</Heading>
+    //   <Search/>
+    //   <CardCategory/>
+    // </Template>
   );
 }
 
