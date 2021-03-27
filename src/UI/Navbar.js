255250a39b75e310fc916190bece4b990c92958c
dianaceciliallc <dianaceciliallerenacastro@gmail.com>
2021-03-27 12:13:11 -0500

    Adding navbar desktop without styles

--- src/UI/Navbar.js
+++ src/UI/Navbar.js
@@ -9,6 +9,14 @@ import { colors } from "../ui";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-device-width: 1224px)'
+  })
+  const isTabletOrMobileDevice = useMediaQuery({
+    query: '(max-device-width: 1224px)'
+  })
+
   const location = useLocation();
   const sections = ["home", "explore", "cart", "favorites", "account"];
   const icons = {
@@ -27,52 +35,75 @@ export default function Navbar() {
   };
 
   return (
-    <ul
-      css={css`
-        position: absolute;
-        background: ${colors.white};
-        bottom: 0;
-        width: 100%;
-        left:0;
-        height: 92px;
-        list-style-type: none;
-        margin: 0;
-        padding: 16px 20px;
-        border-radius:22px 22px 0 0;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        z-index: 10;
-        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-        a {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          text-decoration: none;
-          font-size: 12px;
-          line-height: 14px;
-          color: ${colors.dark0};
-          &.selected {
-            color: ${colors.light1};
-          }
-        }
-      `}
-    >
-      {sections.map((section) => (
-        <li key={section}>
-          <Link
-            className={isSelected(section) ? "selected" : ""}
-            to={section === "home" ? "/home" : `/${section}`}
-          >
-            <Icon
-              type={icons[section]}
-              fill={isSelected(section) ? colors.light1 : "black"}
-              size={20}
-            />
-            {section}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      {isDesktopOrLaptop && <>
+        <p>hola como estas</p>
+        {sections.map((section) => (
+                <li key={section}>
+                  <Link
+                    className={isSelected(section) ? "selected" : ""}
+                    to={section === "home" ? "/home" : `/${section}`}
+                  >
+                    <Icon
+                      type={icons[section]}
+                      fill={isSelected(section) ? colors.light1 : "black"}
+                      size={30}
+                    />
+                    {section}
+                  </Link>
+                </li>
+              ))}
+      </>}
+      {isTabletOrMobileDevice && 
+        <>
+            <ul
+              css={css`
+                position: absolute;
+                background: ${colors.white};
+                bottom: 0;
+                width: 100%;
+                left:0;
+                height: 92px;
+                list-style-type: none;
+                margin: 0;
+                padding: 16px 20px;
+                border-radius:22px 22px 0 0;
+                display: flex;
+                justify-content: space-around;
+                align-items: center;
+                z-index: 10;
+                box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+                a {
+                  display: flex;
+                  flex-direction: column;
+                  align-items: center;
+                  text-decoration: none;
+                  font-size: 12px;
+                  line-height: 14px;
+                  color: ${colors.dark0};
+                  &.selected {
+                    color: ${colors.light1};
+                  }
+                }
+              `}
+            >
+              {sections.map((section) => (
+                <li key={section}>
+                  <Link
+                    className={isSelected(section) ? "selected" : ""}
+                    to={section === "home" ? "/home" : `/${section}`}
+                  >
+                    <Icon
+                      type={icons[section]}
+                      fill={isSelected(section) ? colors.light1 : "black"}
+                      size={20}
+                    />
+                    {section}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+        </>}
+    </>
   );
 }
