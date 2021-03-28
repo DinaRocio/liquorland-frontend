55e3e4c9ef4508894656697fd5e9ac17a239709c
Frank Dominguez <elvisf.dominguez@gmail.com>
2021-03-27 22:59:10 -0500

    Create Drink Slice to store and handle our requests to the API for Drinks data

---
+++ src/features/drinks/drinksSlice.js
@@ -0,0 +1,35 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { DrinkServices } from "../../services/DrinkServices";
+
+const fetchDrink = createAsyncThunk(
+  "drinks/fetchDrink",
+  new DrinkServices().show
+);
+
+const drinksSlice = createSlice({
+  name: "drinks",
+  initialState: {
+    drinks: [],
+    drink: {},
+    status: {},
+  },
+  reducers: {},
+  extraReducers: {
+    [fetchDrink.pending]: (state, _) => {
+      return { ...state, status: { ...state.status, show: "LOADING" } };
+    },
+    [fetchDrink.rejected]: (state, action) => {
+      return { ...state, status: { ...state.status, show: "ERROR" } };
+    },
+    [fetchDrink.fulfilled]: (state, { payload }) => {
+      return {
+        ...state,
+        status: { ...state.status, drink: payload, show: "SUCCESS" },
+      };
+    },
+  },
+});
+
+export default drinksSlice.reducer;
+
+export { fetchDrink };
