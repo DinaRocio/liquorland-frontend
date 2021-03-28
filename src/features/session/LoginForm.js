ba17d357019e2607a38946804c0c9ad97685f351
Dina Rocio <dinarociovq15@hotmail.com>
2021-03-27 22:59:40 -0500

    Add functionality to logout buttom

--- src/features/session/LoginForm.js
+++ src/features/session/LoginForm.js
@@ -19,6 +19,7 @@ export default function LoginForm({ id }) {
 
   return (
     <Styledform onSubmit={handleSubmit} id={id}>
+      <SessionError />
       <LoginInput>
         <label>Email Address</label>
         <input
--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,6 +1,8 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
+import { fetchLogout } from "../features/session/sessionSlice";
 import Template from "../templates/Template";
 import { colors } from "../ui";
 import Icon from "../UI/Icon";
@@ -10,6 +12,14 @@ export default function Account({
   email = "lala@lala.com",
   avatarUrl = "https://www.trickscity.com/wp-content/uploads/2018/06/anonymous-dp-for-boys.jpg",
 }) {
+
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.session.token);
+  
+  if (!token){
+    return <Redirect to="/login" />
+  }
+
   const options = ["myDetails", "location", "payment", "help", "about"];
   const iconsOpt = {
     myDetails: "details",
@@ -52,7 +62,7 @@ export default function Account({
           </li>
         ))}
       </AccountOptions>
-      <LogoutButton>
+      <LogoutButton onClick={() => dispatch(fetchLogout(token))}>
         <Icon type="logout" fill={colors.light2} size={28} />
         Logout
         <span></span>
