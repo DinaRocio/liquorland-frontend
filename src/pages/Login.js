c164f251da0aca9817b3aff6e2456a66572b36c2
Dina Rocio <dinarociovq15@hotmail.com>
2021-03-27 22:57:15 -0500

    Move the login form to a different file

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,23 @@
 import { colors } from "../ui";
 import styled from "@emotion/styled";
-import { Link } from "react-router-dom";
 import header from "../assets/main.png";
 import { Icon, InlineIcon } from "@iconify/react";
 import beerIcon from "@iconify/icons-dashicons/beer";
-import Button from "../UI/Button"
+
+import LoginForm from "../features/session/LoginForm";
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router";
 
 export default function Login() {
+
+  const token= useSelector((state) => state.session.token);
+
+  if(token){
+    sessionStorage.setItem("token", token);
+    return <Redirect to="/home" />
+  }
+
+
   return (
     <LoginContainer>
       <Header>
@@ -17,21 +28,7 @@ export default function Login() {
         <h2>Loging</h2>
         <p>Enter your email and password</p>
       </Titles>
-      <StyledForm>
-        <LoginInput>
-          <label>Email</label>
-          <input placeholder="somebody@mail.com" type="email" />
-        </LoginInput>
-        <LoginInput>
-          <label>Password</label>
-          <input placeholder="*********" type="password" />
-        </LoginInput>
-        <a href="#">Forgot Password?</a>
-        <Button>Log In</Button>
-        <SignupLink>
-          Don't have an account?&nbsp;<Link to="/signup">Signup</Link>
-        </SignupLink>
-      </StyledForm>
+      <LoginForm id="login-form" />
     </LoginContainer>
   );
 }
@@ -77,63 +74,5 @@ const Titles = styled.div`
     font-size: 16px;
     line-height: 15px;
     color: ${colors.gray};
-  }
-`;
-const StyledForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  padding: 0px 25px;
-  & > a {
-    display: flex;
-    text-decoration: none;
-    font-size: 14px;
-    line-height: 108.1%;
-    letter-spacing: 0.05em;
-    color: ${colors.dark0};
-    justify-content: flex-end;
-  }
-`;
-const LoginInput = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  border-bottom: 1px solid ${colors.gray};
-  & > label {
-    font-size: 16px;
-    line-height: 29px;
-    color: ${colors.gray};
-    font-family: "Abel", sans-serif;
-  }
-  & > input {
-    align-items: flex-start;
-    padding: 8px 0px;
-    background: ${colors.white};
-    color: ${colors.dark0};
-    border: none;
-    box-sizing: border-box;
-    border-radius: 8px;
-    &:focus {
-      outline: none;
-    }
-    &::placeholder {
-      color: ${colors.gray};
-    }
-  }
-`;
-const SignupLink = styled.div`
-  display: flex;
-  justify-content: center;
-  font-size: 14px;
-  line-height: 108.1%;
-  letter-spacing: 0.05em;
-  color: ${colors.dark0};
-  & > a {
-    display: flex;
-    text-decoration: none;
-    font-size: 14px;
-    line-height: 108.1%;
-    letter-spacing: 0.05em;
-    color: ${colors.light2};
   }
 `;
