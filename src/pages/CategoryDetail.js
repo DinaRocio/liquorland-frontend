2c616c39539d92bcf6254a8750ccf3b383c7e1bf
Dina Rocio <dinarociovq15@hotmail.com>
2021-04-01 00:28:37 -0500

    Update Category detail page with categories data

--- src/pages/CategoryDetail.js
+++ src/pages/CategoryDetail.js
@@ -1,53 +1,62 @@
 import styled from "@emotion/styled";
-import {Card, Price} from "../components/Card";
-import { useHistory } from "react-router";
+import { Card, Price } from "../components/Card";
+import { useHistory, useParams } from "react-router";
 import Icon from "../UI/Icon";
-import coke from "../assets/coke.png"
+import { colors } from "../ui";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCategory } from "../features/categories/categoriesSlice";
+import { Link } from "react-router-dom";
 export default function CategoryDetail() {
   let history = useHistory();
+  const dispatch = useDispatch();
+  const category = useSelector((state) => state.categories.item);
+  const status = useSelector((state) => state.categories.status);
+  const error = useSelector((state) => state.categories.error);
+  const { category_id } = useParams();
+
+  if (status === "idle") {
+    dispatch(fetchCategory(category_id));
+  }
+
   return (
     <TwoTemplate>
-      <Heading>
-        <Icon
-          type="backArrow"
-          fill="black"
-          size={20}
-          onClick={() => history.goBack()}
-        />
-        Find Products
-        <Icon
-          type="filter"
-          fill="black"
-          size={20}
-          onClick={() => history.push("/filters")}
-        />
-      </Heading>
-      <ProductsContainer>
-      <Card>
-        <img src={coke}/>
-        <h5>Diet Coke</h5>
-        <p>355ml</p>
-        <Price>$1.99</Price>
-      </Card>
-      <Card>
-        <img src={coke}/>
-        <h5>Diet Coke</h5>
-        <p>355ml</p>
-        <Price>$1.99</Price>
-      </Card>
-      <Card>
-        <img src={coke}/>
-        <h5>Diet Coke</h5>
-        <p>355ml</p>
-        <Price>$1.99</Price>
-      </Card>
-      <Card>
-        <img src={coke}/>
-        <h5>Diet Coke</h5>
-        <p>355ml</p>
-        <Price>$1.99</Price>
-      </Card>
-      </ProductsContainer>
+      {status === "loading" && "loading ..."}
+      {status === "error" && "Something went wrong"}
+      {status === "succeeded" && (
+        <>
+          <Heading>
+            <Icon
+              type="backArrow"
+              fill="black"
+              size={20}
+              onClick={() => history.goBack()}
+            />
+            {category.name}
+            <Icon
+              type="filter"
+              fill="black"
+              size={20}
+              onClick={() => history.push("/filters")}
+            />
+          </Heading>
+          <Description>
+            <p>{category.description}</p>
+          </Description>
+          <ProductsContainer>
+            {category.drinks &&
+              category.drinks.map((drink) => (
+                <Card key={drink.id}>
+                  <Link to={`/drinks/${drink.id}`} >
+                    <img src={drink.image_url} />
+                    <h4>{drink.name}</h4>
+                    <p>{drink.presentation}</p>
+                    <Price>${drink.price}</Price>
+                  </Link>
+                </Card>
+              ))}
+          </ProductsContainer>
+        </>
+      )}
     </TwoTemplate>
   );
 }
@@ -67,10 +76,25 @@ const Heading = styled.div`
   display: flex;
   width: 100%;
   justify-content: space-between;
-  margin-top:20px;
+  margin-top: 20px;
+`;
+const Description = styled.div`
+  border-top: 1px solid rgba(226, 226, 226, 0.7);
+  border-bottom: 1px solid rgba(226, 226, 226, 0.7);
+  margin-bottom: 30px;
+  margin-top: 35px;
+  & > p {
+    font-style: italic;
+    font-weight: normal;
+    font-size: 13px;
+    line-height: 21px;
+    color: ${colors.gray};
+    text-align: center;
+  }
 `;
 const ProductsContainer = styled.div`
-width: 100%;
-display: grid;
-grid-template-columns: 1fr 1fr;
-`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 15px;
+`;
