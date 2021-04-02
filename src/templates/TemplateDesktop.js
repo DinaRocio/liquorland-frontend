4be28eeb24e3931963241eb67340c8b3eb59fee4
dianaceciliallc <dianaceciliallerenacastro@gmail.com>
2021-04-02 01:04:57 -0500

    Add styles for templateDesktop

--- src/templates/TemplateDesktop.js
+++ src/templates/TemplateDesktop.js
@@ -1,12 +1,26 @@
+import styled from '@emotion/styled';
 import Navbar from "../UI/Navbar";
 import Footer from "../components/Footer";
 
 export default function TemplateDesktop({children}){
     return(
-        <div>
+        <Container>
             <Navbar/>
               {children}
+            <div className="footer">
             <Footer/>
-        </div>
+            </div>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    display:flex;
+    justify-content:center;
+    flex-direction:column;
+    margin-left:220px;
+    & > .footer {
+        display:flex;
+        justify-content:center;
+    }
+`;
