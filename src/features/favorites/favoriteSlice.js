4efbc6f087597adba5cc7463c5ba1c4da176dd86
Dina Rocio <dinarociovq15@hotmail.com>
2021-04-04 12:38:16 -0500

    Creat all necessary elements to fetch index, create and delete Favorites

---
+++ src/features/favorites/favoriteSlice.js
@@ -0,0 +1,70 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { FavoriteServices } from "../../services/FavoriteServices";
+
+const favoriteService = new FavoriteServices();
+
+const fetchIndexFavorites = createAsyncThunk(
+  "favorite/fetchIndexFavorites",
+  favoriteService.index
+);
+const fetchCreateFavorite = createAsyncThunk(
+  "favorite/fetchCreateFavorite",
+  favoriteService.create
+);
+const fetchDeleteFavorite = createAsyncThunk(
+  "favorite/fetchDeleteFavorite",
+  favoriteService.delete
+);
+
+const favoriteSlice = createSlice({
+  name:"favorite",
+  initialState: {
+    list: [],
+    status: {
+      index: "idle",
+      create: "idle",
+      delete: "idle",
+    },
+    error: null,
+  },
+  extraReducers: {
+    [fetchIndexFavorites.pending]: (state) => {
+      state.status.index = "loading";
+      state.error = null;
+    },
+    [fetchIndexFavorites.fulfilled]: (state, action) => {
+      state.status.index = "success";
+      state.list = action.payload;
+    },
+    [fetchIndexFavorites.rejected]: (state, action) => {
+      state.status.index = "failed";
+      state.error = JSON.parse(action.error.message);
+    },
+    [fetchCreateFavorite.fulfilled]: (state) => {
+      state.status.create = "success";
+      state.error = null;
+    },
+    [fetchCreateFavorite.rejected]: (state) => {
+      state.status.create = "failed";
+      state.error = JSON.parse(action.error.message);
+    },
+    [fetchDeleteFavorite.pending]: (state, _) => {
+      state.status.delete = "loading";
+      state.error = null;
+    },
+    [fetchDeleteFavorite.fulfilled]: (state, {payload}) => {
+      state.status.delete = "success";
+      state.error = null;
+      state.list = state.list.filter((item) => item.id !== payload);
+    },
+    [fetchDeleteFavorite.rejected]: (state, action) => {
+      state.status.delete = "failed";
+      state.error = JSON.parse(action.error.message);
+    },
+  },
+});
+
+export const {removeFavoriteItem } = favoriteSlice.actions;
+export default favoriteSlice.reducer;
+
+export {fetchIndexFavorites, fetchCreateFavorite, fetchDeleteFavorite};
---
+++ src/services/FavoriteServices.js
@@ -0,0 +1,35 @@
+import { BASE_URI } from "../app/config";
+import { APIFetch } from "./apiFetch";
+
+FavoriteServices.prototype.index = async function (token) {
+  return await APIFetch(`${BASE_URI}/api/favorites`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+FavoriteServices.prototype.create = async function ({ token, data }) {
+  return await APIFetch(`${BASE_URI}/api/favorites`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ favorite: data }),
+  });
+};
+
+FavoriteServices.prototype.delete = async function ({ token, favoriteId }) {
+  const result = await APIFetch(`${BASE_URI}/api/favorites/${favoriteId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (result === true) return favoriteId;
+  return result;
+};
+
+export { FavoriteServices };
